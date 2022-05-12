23fd96e7afe058a62d4cccc09bba39aed05c051f
Maia Nguyen <maialyn@csu.fullerton.edu>
2022-05-11 22:54:33 -0700

    typo

--- gui.py
+++ gui.py
@@ -244,7 +244,7 @@ def end(player):
                 
         color = GREEN if player == 1 else RED
 
-        text = BASE_FONT.render((f'Player {player} loses'), True, color)
+        text = BASE_FONT.render((f'Player {player} wins'), True, color)
         text_rect = text.get_rect(center=(SCREEN_WIDTH/2,SCREEN_HEIGHT/2))  
         screen.blit(text, text_rect)
 
