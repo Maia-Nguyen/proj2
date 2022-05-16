59fdf579408b169815758c4cebf0c096c751c19b
Raul Cruz <raulcruz12345@gmail.com>
2022-05-16 00:35:29 -0700

    Fix game of nim and prioritize second player

--- game_of_nim.py
+++ game_of_nim.py
@@ -9,28 +9,20 @@ class GameOfNim(Game):
     def __init__(self, board=[]):
         """ Define goal state and initialize a problem """
         moves = []
-        r = 0
-        for row in board:
-            if row != 0:
-                n = 1
-                while n <= row:
-                    moves.append([r, n])
-                    n += 1
-            r += 1
+
+        for idx, row in enumerate(board):
+            for n in range(1, row + 1):
+                moves.append([idx, n])
+
 
         self.initial = GameState(to_move=1, utility=0, board=board, moves=moves)   
 
     def actions(self, state):
         moves = []
-        r = 0
 
-        for row in state.board:
-            if row != 0:
-                n = 1
-                while n <= row:
-                    moves.append([r, n])
-                    n += 1
-            r += 1
+        for idx, row in enumerate(state.board):
+            for n in range(1, row + 1):
+                moves.append([idx, n])
 
         return moves
         
@@ -38,22 +30,18 @@ class GameOfNim(Game):
         # move = (1,3), remove one from 1st row
         new_board = state.board.copy()
         moves = []
-        r = 0
         
         new_board[move[0]] = state.board[move[0]] - move[1]
 
-        for row in new_board:
-            if row != 0:
-                n = 1
-                while n <= row:
-                    moves.append([r, n])
-                    n += 1
-            r += 1
+        for idx, row in enumerate(new_board):
+                for n in range(1, row + 1):
+                    moves.append([idx, n])
 
         return GameState(to_move=(state.to_move + 1) % 2, utility=0, board=new_board, moves=moves)   
 
+    # The utility function determines which side the max is assigned to.
     def utility(self, state, player):
-        if state.to_move == 'player one':
+        if state.to_move == 1: # ' == 0 ' for prioritizing first player and ' == 1' for second player
             return 1
         else:
             return -1
